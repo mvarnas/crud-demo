5ec3813dab94ee971214e4aff3816d352c1c405d
mvarnas <michelle.varnas@gmail.com>
2018-05-11 19:32:37 -0700

    added models and users.js

--- crud-demo/routes/index.js
+++ crud-demo/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "IS445 Demo" });
+});
+
 module.exports = router;
